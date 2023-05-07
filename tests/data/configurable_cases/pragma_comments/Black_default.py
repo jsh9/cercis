0504ef4fcb2f984e211e84e232c4a4c0799e1c04
jsh9 <25124332+jsh9@users.noreply.github.com>
2023-05-06 19:46:35 -0700

    Adjust test cases to demo Black's default behavior

--- tests/data/configurable_cases/pragma_comments/Black_default.py
+++ tests/data/configurable_cases/pragma_comments/Black_default.py
@@ -19,15 +19,21 @@ a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # noqa:E123, W234, ABC456, XYZ2
 a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # noqa:E123, W234, ABC456, XYZ2
 
+# Black does not wrap a line, no matter how long,
+# if it has "# type: ignore" at the end.
 a_very_long_library_name._private_method(and_a_long_arg)  # type: ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type: ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # type: ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # type: ignore[something]
+a_very_long_library_name._private_method(Though_hundreds_of_thousands_had_done_their_very_best_to_disfigure_the_small_piece_of_land_on_which_they_were_crowded_together_by_paving_the_ground_with_stones_scraping_away_every_vestige_of_vegetation_cutting_down_the_trees_turning_away_birds_and_beasts_and_filling_the_air_with_the_smoke_of_naphtha_and_coal_still_spring_was_spring_even_in_the_town)  # type: ignore[something]
 
+# Black only exempts "# type: ignore" but not "# type:ignore".
+# The latter is also valid for mypy and many people use the latter.
 a_very_long_library_name._private_method(and_a_long_arg)  # type:ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type:ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # type:ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # type:ignore[something]
+a_very_long_library_name._private_method(Though_hundreds_of_thousands_had_done_their_very_best_to_disfigure_the_small_piece_of_land_on_which_they_were_crowded_together_by_paving_the_ground_with_stones_scraping_away_every_vestige_of_vegetation_cutting_down_the_trees_turning_away_birds_and_beasts_and_filling_the_air_with_the_smoke_of_naphtha_and_coal_still_spring_was_spring_even_in_the_town)  # type:ignore[something]
 
 # output
 
@@ -84,11 +90,16 @@ a_very_long_library_name._private_method(
     and_a_long_arg_that_no_long_fits_______
 )  # noqa:E123, W234, ABC456, XYZ2
 
+# Black does not wrap a line, no matter how long,
+# if it has "# type: ignore" at the end.
 a_very_long_library_name._private_method(and_a_long_arg)  # type: ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type: ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # type: ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # type: ignore[something]
+a_very_long_library_name._private_method(Though_hundreds_of_thousands_had_done_their_very_best_to_disfigure_the_small_piece_of_land_on_which_they_were_crowded_together_by_paving_the_ground_with_stones_scraping_away_every_vestige_of_vegetation_cutting_down_the_trees_turning_away_birds_and_beasts_and_filling_the_air_with_the_smoke_of_naphtha_and_coal_still_spring_was_spring_even_in_the_town)  # type: ignore[something]
 
+# Black only exempts "# type: ignore" but not "# type:ignore".
+# The latter is also valid for mypy and many people use the latter.
 a_very_long_library_name._private_method(
     and_a_long_arg
 )  # type:ignore[something]
@@ -100,4 +111,7 @@ a_very_long_library_name._private_method(
 )  # type:ignore[something]
 a_very_long_library_name._private_method(
     and_a_long_arg_that_no_long_fits_______
+)  # type:ignore[something]
+a_very_long_library_name._private_method(
+    Though_hundreds_of_thousands_had_done_their_very_best_to_disfigure_the_small_piece_of_land_on_which_they_were_crowded_together_by_paving_the_ground_with_stones_scraping_away_every_vestige_of_vegetation_cutting_down_the_trees_turning_away_birds_and_beasts_and_filling_the_air_with_the_smoke_of_naphtha_and_coal_still_spring_was_spring_even_in_the_town
 )  # type:ignore[something]
