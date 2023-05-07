29217c6befe55d91a635002f7229636b26386ebf
jsh9 <25124332+jsh9@users.noreply.github.com>
2023-05-06 19:57:24 -0700

    Allow flexible spaces between "type:" and "ignore"

--- tests/data/configurable_cases/pragma_comments/Cercis_default.py
+++ tests/data/configurable_cases/pragma_comments/Cercis_default.py
@@ -1,44 +1,52 @@
 # Some test cases in this file comes from: https://github.com/google/pyink/blob/f93771c02e9a26ce9508c59d69c9337c95797eac/tests/data/pyink/pragma_comments.py
-a_very_long_library_name._private_method(and_a_long_arg)  # pylint: disable=protected-access
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # pylint: disable=protected-access
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # pylint: disable=protected-access
-a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # pylint: disable=protected-access
-
 a_very_long_library_name._private_method(and_a_long_arg)  # pylint:disable=protected-access
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # pylint:disable=protected-access
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # pylint:disable=protected-access
 a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # pylint:disable=protected-access
 
-a_very_long_library_name._private_method(and_a_long_arg)  # noqa: E123, W234, ABC456, XYZ2
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # noqa: E123, W234, ABC456, XYZ2
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # noqa: E123, W234, ABC456, XYZ2
-a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # noqa: E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg)  # pylint: disable=protected-access
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # pylint: disable=protected-access
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # pylint: disable=protected-access
+a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # pylint: disable=protected-access
+
+a_very_long_library_name._private_method(and_a_long_arg)  # pylint:  disable=protected-access
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # pylint:  disable=protected-access
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # pylint:  disable=protected-access
+a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # pylint:  disable=protected-access
 
 a_very_long_library_name._private_method(and_a_long_arg)  # noqa:E123, W234, ABC456, XYZ2
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # noqa:E123, W234, ABC456, XYZ2
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # noqa:E123, W234, ABC456, XYZ2
 a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # noqa:E123, W234, ABC456, XYZ2
 
-a_very_long_library_name._private_method(and_a_long_arg)  # type: ignore[something]
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type: ignore[something]
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # type: ignore[something]
-a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # type: ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg)  # noqa: E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # noqa: E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # noqa: E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # noqa: E123, W234, ABC456, XYZ2
+
+a_very_long_library_name._private_method(and_a_long_arg)  # noqa:  E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # noqa:  E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # noqa:  E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # noqa:  E123, W234, ABC456, XYZ2
 
 a_very_long_library_name._private_method(and_a_long_arg)  # type:ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type:ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # type:ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # type:ignore[something]
 
+a_very_long_library_name._private_method(and_a_long_arg)  # type: ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type: ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # type: ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # type: ignore[something]
+
+a_very_long_library_name._private_method(and_a_long_arg)  # type:  ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type:  ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # type:  ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg_that_no_long_fits_______)  # type:  ignore[something]
+
 # output
 
 # Some test cases in this file comes from: https://github.com/google/pyink/blob/f93771c02e9a26ce9508c59d69c9337c95797eac/tests/data/pyink/pragma_comments.py
-a_very_long_library_name._private_method(and_a_long_arg)  # pylint: disable=protected-access
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # pylint: disable=protected-access
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # pylint: disable=protected-access
-a_very_long_library_name._private_method(
-    and_a_long_arg_that_no_long_fits_______
-)  # pylint: disable=protected-access
-
 a_very_long_library_name._private_method(and_a_long_arg)  # pylint:disable=protected-access
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # pylint:disable=protected-access
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # pylint:disable=protected-access
@@ -46,12 +54,19 @@ a_very_long_library_name._private_method(
     and_a_long_arg_that_no_long_fits_______
 )  # pylint:disable=protected-access
 
-a_very_long_library_name._private_method(and_a_long_arg)  # noqa: E123, W234, ABC456, XYZ2
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # noqa: E123, W234, ABC456, XYZ2
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # noqa: E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg)  # pylint: disable=protected-access
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # pylint: disable=protected-access
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # pylint: disable=protected-access
 a_very_long_library_name._private_method(
     and_a_long_arg_that_no_long_fits_______
-)  # noqa: E123, W234, ABC456, XYZ2
+)  # pylint: disable=protected-access
+
+a_very_long_library_name._private_method(and_a_long_arg)  # pylint:  disable=protected-access
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # pylint:  disable=protected-access
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # pylint:  disable=protected-access
+a_very_long_library_name._private_method(
+    and_a_long_arg_that_no_long_fits_______
+)  # pylint:  disable=protected-access
 
 a_very_long_library_name._private_method(and_a_long_arg)  # noqa:E123, W234, ABC456, XYZ2
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # noqa:E123, W234, ABC456, XYZ2
@@ -60,12 +75,19 @@ a_very_long_library_name._private_method(
     and_a_long_arg_that_no_long_fits_______
 )  # noqa:E123, W234, ABC456, XYZ2
 
-a_very_long_library_name._private_method(and_a_long_arg)  # type: ignore[something]
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type: ignore[something]
-a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # type: ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg)  # noqa: E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # noqa: E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # noqa: E123, W234, ABC456, XYZ2
 a_very_long_library_name._private_method(
     and_a_long_arg_that_no_long_fits_______
-)  # type: ignore[something]
+)  # noqa: E123, W234, ABC456, XYZ2
+
+a_very_long_library_name._private_method(and_a_long_arg)  # noqa:  E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # noqa:  E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # noqa:  E123, W234, ABC456, XYZ2
+a_very_long_library_name._private_method(
+    and_a_long_arg_that_no_long_fits_______
+)  # noqa:  E123, W234, ABC456, XYZ2
 
 a_very_long_library_name._private_method(and_a_long_arg)  # type:ignore[something]
 a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type:ignore[something]
@@ -73,3 +95,17 @@ a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)
 a_very_long_library_name._private_method(
     and_a_long_arg_that_no_long_fits_______
 )  # type:ignore[something]
+
+a_very_long_library_name._private_method(and_a_long_arg)  # type: ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type: ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # type: ignore[something]
+a_very_long_library_name._private_method(
+    and_a_long_arg_that_no_long_fits_______
+)  # type: ignore[something]
+
+a_very_long_library_name._private_method(and_a_long_arg)  # type:  ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit_)  # type:  ignore[something]
+a_very_long_library_name._private_method(and_a_long_arg_that_just_fits_limit___)  # type:  ignore[something]
+a_very_long_library_name._private_method(
+    and_a_long_arg_that_no_long_fits_______
+)  # type:  ignore[something]
